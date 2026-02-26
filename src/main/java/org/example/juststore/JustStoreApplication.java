eea8efca49f493d5d573f09777adf48c7cff5cef
Roman Gulevatiy <gulevatiy.roman@gmail.com>
2026-02-26 11:06:39 +0200

    Initial commit

---
+++ src/main/java/org/example/juststore/JustStoreApplication.java
@@ -0,0 +1,13 @@
+package org.example.juststore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JustStoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JustStoreApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/juststore/JustStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.juststore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JustStoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
